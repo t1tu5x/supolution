a6caae17f628efd00f38bf29f972ec0334572f12
t1tu5x <saveram@tuta.io>
2025-07-28 16:22:04 +0300

    Update soup_game.py

--- soup_game.py
+++ soup_game.py
@@ -61,4 +61,67 @@ class SoupGame:
         # Условие победы: 3 фракции уважают тебя + 1 мегапроект
         if (
             all(v >= 5 for v in self.factions.values()) and
-            "Супознание" in s
+            "Супознание" in self.tech and
+            "Храм Ложки" in self.structures
+        ):
+            self.status = "ascended"
+
+    def trigger_random_event(self):
+        if not self.events:
+            return
+        event = random.choice(self.events)
+        self.events_log.append(event["text"])
+
+        effect = event.get("effect", {})
+        for key, val in effect.get("resources", {}).items():
+            if key in self.resources:
+                self.resources[key] = max(0, self.resources[key] + val)
+
+        for faction, shift in effect.get("factions", {}).items():
+            if faction in self.factions:
+                self.factions[faction] += shift
+
+        if effect.get("hp"):
+            self.hp = max(0, self.hp + effect["hp"])
+
+        if "death" in effect and effect["death"] is True:
+            self.status = "flushed"
+
+    def get_upgrade_choices(self):
+        доступные = [u for u in self.upgrades if u["name"] not in self.tech]
+        return random.sample(доступные, min(3, len(доступные)))
+
+    def apply_upgrade(self, upgrade_name):
+        найден = next((u for u in self.upgrades if u["name"] == upgrade_name), None)
+        if not найден:
+            return False
+
+        for k, v in найден.get("bonus", {}).items():
+            if k in self.resources:
+                self.resources[k] += v
+
+        if "structure" in найден:
+            self.structures.append(найден["structure"])
+
+        for faction, shift in найден.get("factions", {}).items():
+            if faction in self.factions:
+                self.factions[faction] += shift
+
+        self.tech.append(найден["name"])
+
+        if найден.get("win"):
+            self.status = "ascended"
+
+        return True
+
+    def get_state(self):
+        return {
+            "turn": self.turn,
+            "resources": dict(self.resources),
+            "tech": list(self.tech),
+            "hp": self.hp,
+            "status": self.status,
+            "factions": dict(self.factions),
+            "structures": list(self.structures),
+            "events_log": list(self.events_log[-5:])
+        }
